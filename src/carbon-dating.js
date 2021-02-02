b13358f7f684c6c284d30204684cf6609857bf43
olegov-lab <olego8760@gmail.com>
2021-02-02 22:09:53 +0300

    init: carbone done!

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity) {
+
+  if ((sampleActivity == 0) || (sampleActivity == undefined) || isNaN(sampleActivity)
+  || (typeof sampleActivity == 'number') || (sampleActivity > 15)
+  || (sampleActivity < 0) || (Array.isArray(sampleActivity))) {
+    return false;
+  } else {
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let t = Math.ceil(Math.log((MODERN_ACTIVITY / sampleActivity )) / k);
+  return t;
+  }
+
+}
