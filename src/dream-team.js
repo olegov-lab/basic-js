a1391c6572476029d63bfb112dcf3aaec1244a1f
olegov-lab <olego8760@gmail.com>
2021-02-04 00:04:16 +0300

    init: done!

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+
+ if ((members == null) || (members == undefined)) {
+
+    return false;
+}
+
+let array = '';
+
+for (i = 0; i < members.length; i++) {
+
+if (typeof members[i] == 'string') {
+array = array + members[i].trim()[0];
+}
+}
+
+array = array.toUpperCase();
+array = array.split('').sort();
+array = array.join('');
+return array;
+}
+
+
