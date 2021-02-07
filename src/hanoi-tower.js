efceb7c4454a1b4df511ead3f820032962af32a1
olegov-lab <olego8760@gmail.com>
2021-02-07 17:43:54 +0300

    init: hanoi done!

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+
+    let newO = {};
+    newO.turns = Math.pow(2,disksNumber) - 1;
+    newO.seconds = Math.floor(newO.turns * 3600 / turnsSpeed);
+    return newO;
+    //console.log(obj.turns);
+      //throw new CustomError('Not implemented');
+      // remove line with error and write your code here
+    }
+
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
-};
+
