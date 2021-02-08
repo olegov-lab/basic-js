314de1aeff3c591e52eb218f8150628c18d80c3c
olegov-lab <olego8760@gmail.com>
2021-02-08 22:25:26 +0300

    init: recursiv done!

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
+  calculateDepth(arr) {
+
+    if (arr.length === 0) {
+      return 1;
+      }
+
+      if (Array.isArray(arr)) {
+      return 1 + Math.max(...arr.map(t => this.calculateDepth(t)));
+      } else return 0;
+
+   }
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
-  }
-};
+
+}
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,72 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  let readyArr = [...arr];
+  const discPrev = '--discard-prev',
+        discNext = '--discard-next',
+        doubPrev = '--double-prev',
+        doubNext = '--double-next';
+
+
+function discNextF() {
+  if (readyArr[readyArr.indexOf(discNext)+2] === doubPrev || readyArr[readyArr.indexOf(discNext)+2] === discPrev){
+    readyArr.splice(readyArr.indexOf(discNext), 3)
+  } else if(readyArr.indexOf(discNext) == readyArr.length-1){
+    readyArr.splice(readyArr.indexOf(discNext), 1)
+  } else if (readyArr.indexOf(discNext) == -1) {
+
+  }  else {
+    readyArr.splice(readyArr.indexOf(discNext), 2)
+  }
+}
+
+function discPrevF() {
+  if(readyArr.indexOf(discPrev) == 0) {
+    readyArr.splice(readyArr.indexOf(discPrev), 1)
+  } else if (readyArr.indexOf(discPrev)==-1) {
+
+  } else{
+    readyArr.splice(readyArr.indexOf(discPrev)-1, 2)
+  }
+}
+
+  function doubNextF() {
+  if(readyArr.indexOf(doubNext) == readyArr.length-1) {
+    readyArr.splice(readyArr.indexOf(doubNext), 1)
+  } else if (readyArr.indexOf(doubNext) == -1) {
+
+  } else{
+    readyArr[readyArr.indexOf(doubNext)] = readyArr[readyArr.indexOf(doubNext)+1]
+  }
+}
+
+  function doubPrevF() {
+  if (readyArr.indexOf(doubPrev) == 0) {
+    readyArr.splice(readyArr.indexOf(doubPrev), 1)
+  } else if (readyArr.indexOf(doubPrev) == -1) {
+
+  } else{
+    readyArr[readyArr.indexOf(doubPrev)] = readyArr[readyArr.indexOf(doubPrev)-1];
+  }
+}
+for(let i = 0; i < arr.length; i++) {
+    if (arr[i] == discNext) {
+      if (arr[i+2] === doubPrev || arr[i+2] === discPrev){
+        i+=3;
+      }
+      discNextF();
+    }
+    if (arr[i] == discPrev) {
+      discPrevF();
+    }
+    if (arr[i] == doubNext) {
+      doubNextF();
+    }
+    if (arr[i] == doubPrev) {
+      doubPrevF();
+    } else {
+
+    }
+}
+  return readyArr;
+}
