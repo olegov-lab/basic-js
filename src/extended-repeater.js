06caa8dbae5612c6d62b35265d7e2ae89fde9e1f
olegov-lab <olego8760@gmail.com>
2021-02-28 14:40:45 +0300

    init:clear

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,43 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-	str = String(str);
 
-	let additionArr = [], additionStr = '', additionRepeatTimes = 1, separator = '+', additionSeparator = '', repeatTimes = 1;
-
-	if (options !== undefined && options.addition !== undefined) {
-		additionStr = String(options.addition);
-	}
-
-	if (options !== undefined && options.additionRepeatTimes !== undefined) {
-		additionRepeatTimes = options.additionRepeatTimes;
-	}
-
-	if (options !== undefined && options.separator !== undefined) {
-		separator = options.separator;
-	}
-
-	if (options !== undefined && options.additionSeparator !== undefined) {
-		additionSeparator = options.additionSeparator;
-	}
-
-	if (options !== undefined && options.repeatTimes !== undefined) {
-		repeatTimes = options.repeatTimes;
-	}
-
-	for (let i = 0; i < additionRepeatTimes; i++) {
-		additionArr.push(additionStr);
-	}
-
-	additionStr = additionArr.join(additionSeparator);
-
-	str = str + additionStr;
-
-	strArray = [];
-
-	for (let i = 0; i < repeatTimes; i++) {
-		strArray.push(str);
-	}
-
-	return strArray.join(separator);
 };
