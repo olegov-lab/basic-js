0b154bc6dd4a4b0facc3ca5e85b99f2597db6b0a
olegov-lab <olego8760@gmail.com>
2021-03-02 23:49:25 +0300

    feat:transform-end

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(arr) {
+module.exports = function transform(array) {
 
-}
+    if (!Array.isArray(array)) {
+        throw new Error();
+        }
+
+    let arr = array.slice();
+
+    let dcNext = '--discard-next',
+        dcPrev = '--discard-prev',
+        dbNext = '--double-next',
+        dbPrev = '--double-prev';
+
+    for (let i = 0; i < arr.length; i++) {
+
+    if ( (i == 0) && (arr[i] === dcPrev || arr[i] === dbPrev) ) {
+    continue;
+    } else if ( (i == arr.length - 1) && (arr[i] === dcNext || arr[i] === dbNext) ) {
+    continue;
+    } else if (arr[i] == dcNext) {
+        arr.splice(i + 1, 1);
+    } else if (arr[i] == dcPrev) {
+        arr.splice(i - 1, 1);
+        i--;
+    } else if (arr[i] == dbNext) {
+        arr.splice(i + 1, 0, arr[i + 1]);
+    } else if (arr[i] == dbPrev) {
+        arr.splice(i - 1, 0, arr[i - 1]);
+        i++;
+    }
+    }
+
+    for (let i = 0, len = arr.length; i < len; i++) {
+    if (arr[i] == dcNext || arr[i] == dcPrev || arr[i] == dbNext || arr[i] == dbPrev ) {
+    arr.splice(i,1);
+    i--;
+    }
+    }
+    return arr;
+    };
