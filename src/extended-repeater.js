2d9707c025338aabcf1ad9ddf9ddba71c61bf432
olegov-lab <olego8760@gmail.com>
2021-03-03 23:24:42 +0300

    init:push-extended

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, {
+repeatTimes = 1,
+separator = '+',
+addition = '',
+additionRepeatTimes = 1,
+additionSeparator = '|',
+}) {
+
+  str = String(str);
+  let strBuffer = str;
+
+  if (addition !== undefined) {
+    addition = String(addition);
+    let additionBuffer = addition;
+
+    for (let i = 1; i < additionRepeatTimes; i++) {
+      addition = addition + additionSeparator + additionBuffer;
+    }
+    str = str + addition;
+    strBuffer = str;
+    }
+
+    for (let i = 1; i < repeatTimes; i++) {
+      str = str + separator + strBuffer;
+    }
+    return str;
+  };
+
+
